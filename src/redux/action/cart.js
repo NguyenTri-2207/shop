2e64d9dc50365cd2f4e99d4a14cc6b47b8a68b02
Tri Nguyen <tri.nguyen@gleads.vn>
2023-06-19 18:24:33 +0700

    add

--- src/redux/action/cart.js
+++ src/redux/action/cart.js
@@ -1,12 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-function removeObjectWithId(arr, id) {
-  // Making a copy with the Array from() method
-  const arrCopy = Array.from(arr);
 
-  const objWithIdIndex = arrCopy.findIndex((obj) => obj.id === id);
-  arrCopy.splice(objWithIdIndex, 1);
-  return arrCopy;
-}
 const cartSlice = createSlice({
   name: "cart",
   initialState: [],
